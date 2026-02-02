fa1e4919d4e5166bd71ec55e65d5974561477553
Documenter.jl <documenter@juliadocs.github.io>
2026-02-02 15:00:06 +0000

    build based on bccd1bf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Rhea","page":"Home","title":"Rhea","text":"Documentation for Rhea.\n\n","category":"section"},{"location":"#Rhea.profit-Tuple{Position, Book}","page":"Home","title":"Rhea.profit","text":"Unrealized profit\n\n\n\n\n\n","category":"method"},{"location":"#Rhea.value-Tuple{Position}","page":"Home","title":"Rhea.value","text":"value or cost of a position\n\n\n\n\n\n","category":"method"}]
+}
